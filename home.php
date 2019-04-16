5af1bb269926debfb2482221067694377852d0e4
Lauren Toth <laurentoth@Laurens-MacBook-Pro.local>
2019-04-16 11:27:14 -0400

    Uploading the home page

---
+++ home.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Book Store Browse</title>
+</head>
+<body>
+<P>
+<a href="allBooks.php">See All Books</a>
+<P>
+<a href="allCustomers.php">See All Customers</a>
+<P>
+<a href="allOrders.php">See All Orders</a>
+<P>
+<a href="allMagazines.php">See All Magazines</a>
+<P>
+<a href="allAuthors.php">See All Authors</a>
+
+</body>
+
+</html>
