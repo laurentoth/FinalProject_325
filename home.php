35f9c50a5c58c6e63d4e3c019e2c7015720fadc1
Lauren Toth <laurentoth@Laurens-MacBook-Pro.local>
2019-04-16 17:16:41 -0400

    Finished home page

--- home.php
+++ home.php
@@ -3,17 +3,38 @@
 <title>Book Store Browse</title>
 </head>
 <body>
+<?php $image_url='graph.jpg'; ?>
+<?php $book_image ='header';?>
+<center>
+<table border="2" cellspacing="20" cellpadding="50" style="border:5px solid rgba(255,255,255,.5)">
+<img src="<?php echo $book_image;?>" >
+
+<tr style="border:5px solid rgba(255,255,255,.5)">
+<td style="border:5px solid rgba(255,255,255,.5);">
+<center>
+
 <P>
 <a href="allBooks.php">See All Books</a>
+</center>
+</td>
+<td style="border:5px solid rgba(255,255,255,.5)">
 <P>
 <a href="allCustomers.php">See All Customers</a>
-<P>
-<a href="allOrders.php">See All Orders</a>
-<P>
-<a href="allMagazines.php">See All Magazines</a>
+</td>
+<td style="border:5px solid rgba(255,255,255,.5)">
 <P>
 <a href="allAuthors.php">See All Authors</a>
-
+</td>
+<td style="border:5px solid rgba(255,255,255,.5)">
+<P>
+<a href="allMagazines.php">See All Magazines</a>
+</td>
+<td style="border:5px solid rgba(255,255,255,.5)">
+<P>
+<a href="allOrders.php">See All Orders</a>
+</td>
+</table>
+</center>
 </body>
 
 </html>
